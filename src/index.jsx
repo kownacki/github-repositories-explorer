8efa4b195c3e828dac511649c08cc1dad545f171
Michał Kownacki <mk@michalkownacki.pl>
2020-07-04 16:39:16 +0200

    Add github search based on input.

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInputText: '',
+      searchResponse: null,
+    }
+  }
   render() {
     return (
       <div>
-        <input type="text" className="search-input"></input>
-        <button className="search-button">Search</button>
+        <input
+          className="search-input"
+          onInput={(event) => this.setState({searchInputText: event.target.value})}
+          type="text">
+        </input>
+        <button className="search-button" onClick={async () => {
+          const q = this.state.searchInputText.replace(' ', '+');
+          const response = await fetch(`https://api.github.com/search/users?q=${q}&per_page=5`);
+          this.state.searchResponse = await response.json();
+          console.log(this.state.searchResponse);
+        }}>Search</button>
       </div>
     );
   }
