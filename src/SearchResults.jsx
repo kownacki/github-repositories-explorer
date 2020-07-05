ef6cce15bfdd7dfd272f6dd98e9d84e23b094484
Michał Kownacki <mk@michalkownacki.pl>
2020-07-05 19:55:08 +0200

    Add listing users.

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 export default class SearchResults extends React.Component {
   constructor(props) {
     super(props);
@@ -10,6 +9,7 @@ export default class SearchResults extends React.Component {
   async componentDidUpdate(prevProps) {
     if (!prevProps || this.props.searchQuery !== prevProps.searchQuery) {
       const query = this.props.searchQuery.replace(' ', '+');
+      // todo race condition
       const response = await fetch(`https://api.github.com/search/users?q=${query}&per_page=5`);
       this.setState({searchResponse: await response.json()});
     }
@@ -18,6 +18,11 @@ export default class SearchResults extends React.Component {
     return (
       <div>
         Showing users for "{this.props.searchQuery}"
+        <ul>
+          {this.state.searchResponse && this.state.searchResponse.items.map((item) =>
+            <li key={item.id}>{item.login}</li>
+          )}
+        </ul>
         <br />
       </div>
     );
