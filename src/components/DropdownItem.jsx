ae87863c05a7f21082b9a5d61e74cc4c1e2bb008
Michał Kownacki <mk@michalkownacki.pl>
2020-07-06 02:50:33 +0200

    Style DropdownItem.

--- src/components/DropdownItem.jsx
+++ src/components/DropdownItem.jsx
@@ -4,11 +4,18 @@ import {AiFillStar} from 'react-icons/ai';
 export default class DropdownItem extends React.Component {
   render() {
     return (
-      <div>
-        {this.props.repo.name}
-        <br />
-        {this.props.repo.description}
-        {this.props.repo.watchers} <AiFillStar />
+      <div className="dropdown-item">
+        <div className="dropdown-item-header">
+          <div className="dropdown-item-header-left">
+            {this.props.repo.name}
+          </div>
+          <div className="dropdown-item-header-right">
+            {this.props.repo.watchers} <AiFillStar />
+          </div>
+        </div>
+        <div className="dropdown-item-body">
+          {this.props.repo.description}
+        </div>
       </div>
     );
   }
