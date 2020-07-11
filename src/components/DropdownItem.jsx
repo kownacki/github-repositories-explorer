5bf452afce1d46ac0a1c3a1d2f4ba1be911b6eda
Michał Kownacki <mk@michalkownacki.pl>
2020-07-11 12:07:49 +0200

    Use destructuring assignment.

--- src/components/DropdownItem.jsx
+++ src/components/DropdownItem.jsx
@@ -3,18 +3,19 @@ import {AiFillStar} from 'react-icons/ai';
 
 export default class DropdownItem extends React.Component {
   render() {
+    const {repo} = this.props;
     return (
       <div className="dropdown-item">
         <div className="dropdown-item-header">
           <div className="dropdown-item-header-left">
-            {this.props.repo.name}
+            {repo.name}
           </div>
           <div className="dropdown-item-header-right">
-            {this.props.repo.watchers} <AiFillStar />
+            {repo.watchers} <AiFillStar />
           </div>
         </div>
         <div className="dropdown-item-body">
-          {this.props.repo.description}
+          {repo.description}
         </div>
       </div>
     );
